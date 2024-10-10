8ab68f4ecfac05c17a70221706789fef2b332f5c
Christian Teguim <cteguimtu620@gmail.com>
2024-10-10 14:35:51 -0400

    HealthManager script

---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthManager : MonoBehaviour
+{
+    public int playerHealth = 3;
+    public Text healthText;
+
+    void Start()
+    {
+        UpdateHealthUI(); 
+    }
+
+    // reduce player health by 1
+    public void TakeDamage()
+    {
+        playerHealth--;
+        UpdateHealthUI();
+
+        if (playerHealth <= 0)
+        {
+            Debug.Log("Game Over! Player is out of lives.");
+        }
+    }
+
+    // Method to update the health UI
+    void UpdateHealthUI()
+    {
+        healthText.text = "Lives: " + playerHealth;
+    }
+}
