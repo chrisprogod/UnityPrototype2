3b8d7bfd9c6c2c7b5863e0b6757b5a7fb57a8b95
Christian Teguim <cteguimtu620@gmail.com>
2024-10-10 15:10:57 -0400

    fixed text varaible type

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthManager : MonoBehaviour
 {
     public int playerHealth = 3;
-    public Text healthText;
+    public TextMeshProUGUI healthText;
 
     void Start()
     {
